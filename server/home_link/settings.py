bc3519852d855836ab645282ae1f166c1cad1526
Ishanka D Senevirathne <IshankaDSenevirathne@gmail.com>
2024-11-04 13:53:08 +0530

    get secrets from env

--- server/home_link/settings.py
+++ server/home_link/settings.py
@@ -10,17 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+
+import environ
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Read envs
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+
+env = environ.Env(SECRET_KEY=(str, ""))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "XXXX"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
